f1fb9f12ef2753e3f3de019f45b784dc4e0b6f39
Patrick Steele-Idem <pnidem@gmail.com>
2015-09-06 17:01:56 -0600

    Register "sass" file extension

--- lasso-sass-plugin.js
+++ lasso-sass-plugin.js
@@ -11,50 +11,52 @@ if (sassPath) {
 var extend = require('raptor-util/extend');
 
 module.exports = function(lasso, config) {
-    lasso.dependencies.registerStyleSheetType(
-        'scss',
-        {
-            properties: {
-                'path': 'string',
-                'paths': 'string'
-            },
 
-            init: function(lassoContext, callback) {
-                if (!this.path) {
-                    return callback(new Error('"path" is required for a sass dependency'));
-                }
+    var sassHandler = {
+        properties: {
+            'path': 'string',
+            'paths': 'string'
+        },
 
-                if (!sass) {
-                    return callback(new Error('Unable to handle Sass dependency for path "' + this.path + '". The "node-sass" module was not found. This module should be installed as a top-level application dependency using "npm install node-sasss --save".'));
-                }
-
-                this.path = this.resolvePath(this.path);
-
-                callback();
-            },
-
-            read: function(lassoContext, callback) {
-                var path = this.path;
-
-                var renderOptions = extend({}, config);
-
-                renderOptions.file = path;
-
-                sass.render(renderOptions, function(error, result) {
-                    if (error) {
-                        callback(error);
-                    } else {
-                        callback(null, result.css);
-                    }
-                });
-            },
-
-            getSourceFile: function() {
-                return this.path;
-            },
-
-            getLastModified: function(lassoContext, callback) {
-                return callback(null, -1);
+        init: function(lassoContext, callback) {
+            if (!this.path) {
+                return callback(new Error('"path" is required for a sass dependency'));
             }
-        });
+
+            if (!sass) {
+                return callback(new Error('Unable to handle Sass dependency for path "' + this.path + '". The "node-sass" module was not found. This module should be installed as a top-level application dependency using "npm install node-sasss --save".'));
+            }
+
+            this.path = this.resolvePath(this.path);
+
+            callback();
+        },
+
+        read: function(lassoContext, callback) {
+            var path = this.path;
+
+            var renderOptions = extend({}, config);
+
+            renderOptions.file = path;
+
+            sass.render(renderOptions, function(error, result) {
+                if (error) {
+                    callback(error);
+                } else {
+                    callback(null, result.css);
+                }
+            });
+        },
+
+        getSourceFile: function() {
+            return this.path;
+        },
+
+        getLastModified: function(lassoContext, callback) {
+            return callback(null, -1);
+        }
+    };
+
+    lasso.dependencies.registerStyleSheetType('scss', sassHandler);
+    lasso.dependencies.registerStyleSheetType('sass', sassHandler);
 };
